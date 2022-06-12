e6938ef415737e20cec24695ad37d9757f23e551
Waleed <62436128+WaliiidAls@users.noreply.github.com>
2022-06-12 17:33:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0];
+
+let all_grades_div = document.getElementById("all-grades");
+let search_grades_div = document.getElementById("search-grades");
+let A_grades_div = document.getElementById("A-grades");
+let bonus_grades_div = document.getElementById("bonus-grades");
+let random_grade_div = document.getElementById("random-grade");
