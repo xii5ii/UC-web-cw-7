596da0db68ca1cc629d4c67ae6dc87ae00721d27
Athbi <70595484+Athbi989@users.noreply.github.com>
2022-08-31 15:18:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0];
+/* ------------------------------------------ */
+let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0]; //المصفوفة
 
 let all_grades_div = document.getElementById("all-grades");
-let search_grades_div = document.getElementById("search-grades");
-let A_grades_div = document.getElementById("A-grades");
-let bonus_grades_div = document.getElementById("bonus-grades");
 let random_grade_div = document.getElementById("random-grade");
+/* لا تقم بتعديل الأسطر المكتوبة بالأعلى */
 
 
-function search() {
-  let inputValue = document.getElementById("input").value;
-  
+/*  الجزء الأول والثاني من التمرين */
+
+
+
+
+
+
+
+
+
+/* الجزء الثالث من التمرين */
+
+let course = {
+  name: "UniCODE",
+  track : "",
+  studentsCount : 0,
+  location : "Kuwait University"
 }
+
+//Change studentsCount,track property here
+
+
+
+//Console log here
+
+
+
+//Create student object here
+
